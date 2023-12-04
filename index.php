7565677b1f776cb6ee5cf952cb98121b0db15c1b
Kajofzo <108760962+Kajofzo@users.noreply.github.com>
2023-12-04 11:53:02 +0100

    0.0.1

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body class="bg-black">
+<pre><main class="bg-black text-green-500  ">
+
+____  __.           __  __________                                            .___
+|    |/ _|____      |__| \______   \ ____   ____   _________  _____ _______  __| _/
+|      < \__  \     |  |  |    |  _//  _ \ /  _ \ / ___\__  \ \__  \\_  __ \/ __ | 
+|    |  \ / __ \_   |  |  |    |   (  <_> |  <_> ) /_/  > __ \_/ __ \|  | \/ /_/ | 
+|____|__ (____  /\__|  |  |______  /\____/ \____/\___  (____  (____  /__|  \____ | 
+        \/    \/\______|         \/             /_____/     \/     \/           \/ 
+Kaj/Downloads/php> php -S localhost:8000
+PHP 7.4.3 Development Server (http://localhost:8000) started
+ 
+
+
+</main></pre>
+</body>
+</html>
